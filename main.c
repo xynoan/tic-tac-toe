f7b161b2ad0dafd86c7761bc4b99ab2b0d338a5d
Nathaniel Carlo <100833190+xynoan@users.noreply.github.com>
2023-11-11 20:11:15 +0800

    remove fn prototypes, replace `fgets` with `getline`

--- main.c
+++ main.c
@@ -1,48 +1,13 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void initBoard();
-void printBoard();
-void removeInputNewLine();
-void validateIfNumber();
-void play();
-int checkIfWon();
-
-char board[4][4], input[25], currentplayer;
-int inputLength, isNumber, row, column, playerNo;
-
-int main() {
-  currentplayer = 'X';
-  playerNo = 1;
-  initBoard();
-  printf("\nWelcome to Tic Tac Toe!\n");
-  while (1) {
-    printBoard();
-    if (checkIfWon()) {
-      printf("Player %d has won!\n\n", playerNo);
-      break;
-    }
-    printf("Player %d, enter your move (row column): ", playerNo);
-    fgets(input, sizeof(input), stdin);
-    removeInputNewLine();
-    validateIfNumber();
-
-    if (strcmp(input, "exit") == 0) {
-      printf("Exiting the game.\n\n");
-      break;
-    }
-
-    play();
-
-    if (!isNumber || inputLength > 2 || row > 3 || column > 3 ||
-        inputLength < 2) {
-      printf("Invalid input!\n");
-      continue;
-    }
-  }
-  return 0;
-}
+char board[4][4], currentplayer;
+ssize_t inputLength;
+size_t bufferSize = 0;
+char *input = NULL;
+int row, column, playerNo, isNumber;
 
 void initBoard() {
   board[0][0] = ' ';
@@ -91,26 +56,6 @@ void validateIfNumber() {
   }
 }
 
-void play() {
-  row = input[0] - '0';
-  column = input[1] - '0';
-
-  if (board[row][column] == '_') {
-    board[row][column] = currentplayer;
-    if (!checkIfWon()) {
-      if (currentplayer == 'X') {
-        playerNo = 2;
-        currentplayer = 'O';
-      } else if (currentplayer == 'O') {
-        playerNo = 1;
-        currentplayer = 'X';
-      }
-    }
-  } else {
-    printf("That spot is already taken!\n");
-  }
-}
-
 int checkIfWon() {
   // player 1 - horizontal
   if ((board[1][1] == 'X' && board[1][2] == 'X' && board[1][3] == 'X') ||
@@ -151,6 +96,61 @@ int checkIfWon() {
       (board[3][1] == 'O' && board[2][2] == 'O' && board[1][3] == 'O')) {
     return 1;
   }
+
+  return 0;
+}
+
+void play() {
+  row = input[0] - '0';
+  column = input[1] - '0';
+
+  if (board[row][column] == '_') {
+    board[row][column] = currentplayer;
+    if (!checkIfWon()) {
+      if (currentplayer == 'X') {
+        playerNo = 2;
+        currentplayer = 'O';
+      } else if (currentplayer == 'O') {
+        playerNo = 1;
+        currentplayer = 'X';
+      }
+    }
+  } else {
+    printf("That spot is already taken!\n");
+  }
+}
+
+int main() {
+  currentplayer = 'X';
+  playerNo = 1;
+  initBoard();
+  printf("\nWelcome to Tic Tac Toe!\n");
+  while (1) {
+    printBoard();
+    if (checkIfWon()) {
+      printf("Player %d has won!\n\n", playerNo);
+      break;
+    }
+    printf("Player %d, enter your move (row column): ", playerNo);
+    inputLength = getline(&input, &bufferSize, stdin);
+    removeInputNewLine();
+    validateIfNumber();
+
+    if (strcmp(input, "exit") == 0) {
+      printf("Exiting the game.\n\n");
+      break;
+    }
+
+    play();
+
+    if (!isNumber || inputLength > 2 || row > 3 || column > 3 ||
+        inputLength < 2) {
+      printf("Invalid input!\n");
+      continue;
+    }
+  }
+
+  free(input);
 
   return 0;
 }
